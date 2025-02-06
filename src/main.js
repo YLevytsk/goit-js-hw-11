51af547d3ac998e0484a9c8081c27279624bcaaf
yLevytsk <Lupin773311asd@gmail.com>
2025-02-06 10:31:31 +0000

    rgg

--- src/main.js
+++ src/main.js
@@ -1,17 +1,32 @@
-import { fetchImages } from '/js/pixabay-api.js';
-import { renderImages } from '/js/render-functions.js';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-const form = document.querySelector('.search-form');
+import { fetchImages } from "/js/pixabay-api.js";
+import { renderImages } from "/js/render-functions.js";
 
-form.addEventListener('submit', async event => {
+const form = document.querySelector(".search-form");
+
+form.addEventListener("submit", async event => {
   event.preventDefault();
 
   const query = event.target.elements.searchQuery.value.trim();
   if (!query) {
-    alert('Введите слово для поиска!');
+    iziToast.warning({
+      title: "Ошибка",
+      message: "Введите слово для поиска!",
+      position: "topRight",
+    });
     return;
   }
 
-  const images = await fetchImages(query);
-  renderImages(images);
+  try {
+    const images = await fetchImages(query);
+    renderImages(images);
+  } catch (error) {
+    iziToast.error({
+      title: "Ошибка",
+      message: "Не удалось загрузить изображения. Попробуйте ещё раз.",
+      position: "topRight",
+    });
+  }
 });
