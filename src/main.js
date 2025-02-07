67012627ea6a7f2effc5c247ac8dbf37f5905447
yLevytsk <Lupin773311asd@gmail.com>
2025-02-07 09:53:29 +0000

    впв

--- src/main.js
+++ src/main.js
@@ -4,47 +4,4 @@ import "izitoast/dist/css/iziToast.min.css";
 import { fetchImages } from "/js/pixabay-api.js";
 import { renderImages } from "/js/render-functions.js";
 
-// Функция для отображения индикатора
-function showLoading() {
-  const loadingOverlay = document.getElementById("loading-overlay");
-  loadingOverlay.classList.add("show"); // Добавляем класс для отображения
-}
 
-// Функция для скрытия индикатора
-function hideLoading() {
-  const loadingOverlay = document.getElementById("loading-overlay");
-  loadingOverlay.classList.remove("show"); // Убираем класс для скрытия
-}
-
-const form = document.querySelector(".search-form");
-
-form.addEventListener("submit", async event => {
-  event.preventDefault();
-
-  const query = event.target.elements.searchQuery.value.trim();
-  if (!query) {
-    iziToast.warning({
-      title: "Ошибка",
-      message: "Введите слово для поиска!",
-      position: "topRight",
-    });
-    return;
-  }
-
-  // Показываем индикатор загрузки при отправке формы
-  showLoading();
-
-  try {
-    const images = await fetchImages(query); // Запрос изображений
-    renderImages(images); // Отображение изображений
-  } catch (error) {
-    iziToast.error({
-      title: "Ошибка",
-      message: "Не удалось загрузить изображения. Попробуйте ещё раз.",
-      position: "topRight",
-    });
-  } finally {
-    // Скрываем индикатор загрузки после завершения запроса
-    hideLoading();
-  }
-});
